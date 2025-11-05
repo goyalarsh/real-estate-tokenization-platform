d8dcf4c21bc1353baaf50b10316d4388760e6de9
Arsh Goyal <arshgoyal2005@gmail.com>
2025-11-05 17:51:50 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,27 +1,23 @@
-export default {
-  solidity: {
-    version: "0.8.20",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200,
-      },
-      viaIR: true, // <-- Add this line
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+require("dotenv").config();
+
+module.exports = {
+  solidity: "0.8.20",
+  networks: {
+    coreTestnet: {
+      url: "https://rpc.test2.btcs.network",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+      chainId: 1114,
     },
   },
   paths: {
+    artifacts: "./artifacts",
+    cache: "./cache",
     sources: "./contracts",
-    tests: "./contracts/test",
-    cache: "./contracts/cache",
-    artifacts: "./web/src/contracts/artifacts",
-  },
-  typechain: {
-    outDir: "web/src/contracts/types",
-    target: "ethers-v6",
-  },
-  networks: {
-    localhost: {
-      url: "http://127.0.0.1:8545",
-    },
+    tests: "./test",
   },
 };
